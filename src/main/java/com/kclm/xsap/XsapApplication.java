8fdd3adf28dfaf89a5bc90f2971c5a05a03539d6
yejf <51467976@qq.com>
2020-09-03 14:45:14 +0800

    update project develop structure

---
+++ src/main/java/com/kclm/xsap/XsapApplication.java
@@ -0,0 +1,13 @@
+package com.kclm.xsap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XsapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XsapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kclm/xsap/XsapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kclm.xsap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XsapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
