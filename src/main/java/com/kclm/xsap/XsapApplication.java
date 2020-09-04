cad9e3571b54fc6904dae7dcf397bbcebc359e2f
yejf <51467976@qq.com>
2020-09-04 08:48:43 +0800

    update project structure

--- src/main/java/com/kclm/xsap/XsapApplication.java
+++ src/main/java/com/kclm/xsap/XsapApplication.java
@@ -1,9 +1,11 @@
 package com.kclm.xsap;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.kclm.xsap.mapper")
 public class XsapApplication {
 
     public static void main(String[] args) {
