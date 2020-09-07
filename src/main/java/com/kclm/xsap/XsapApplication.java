87978b2107b114b26aba9e5006dbcadb2cc334ea
yejf <51467976@qq.com>
2020-09-07 16:15:32 +0800

    update

--- src/main/java/com/kclm/xsap/XsapApplication.java
+++ src/main/java/com/kclm/xsap/XsapApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.kclm.xsap.mapper")
 public class XsapApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/kclm/xsap/XsapApplicationTests.java
+++ src/test/java/com/kclm/xsap/XsapApplicationTests.java
@@ -1,13 +1,20 @@
 package com.kclm.xsap;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
 
 @SpringBootTest
 class XsapApplicationTests {
 
+    @Autowired
+    private DataSource dataSource;
+
     @Test
     void contextLoads() {
+        System.out.println(dataSource);
     }
 
 }
