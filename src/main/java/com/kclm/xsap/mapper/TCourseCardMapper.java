f1ca11e3e40d75855357b7f87e2d0473e3ce3164
HarimaSal <salos135@163.com>
2020-09-08 14:24:53 +0800

    modify_6

--- src/main/java/com/kclm/xsap/mapper/TCourseCardMapper.java
+++
@@ -1,9 +0,0 @@
-package com.kclm.xsap.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.kclm.xsap.entity.TCourseCard;
-import org.apache.ibatis.annotations.Param;
-
-public interface TCourseCardMapper extends BaseMapper {
-
-}
