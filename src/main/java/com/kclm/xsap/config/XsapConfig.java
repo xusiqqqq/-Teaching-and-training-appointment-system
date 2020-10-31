47713eecc315ef91a13789001c2df9c3f2f3d406
yejf <51467976@qq.com>
2020-10-31 15:39:45 +0800

    fix bug

--- src/main/java/com/kclm/xsap/config/XsapConfig.java
+++ src/main/java/com/kclm/xsap/config/XsapConfig.java
@@ -1,34 +1,27 @@
 package com.kclm.xsap.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Component
 @PropertySource("classpath:xsap-config.properties")
+@ConfigurationProperties(prefix = "page.reservation")
 public class XsapConfig {
 
 	private Long gap_minute;
-	
+
 	public XsapConfig() {
 		super();
 		System.out.println("分钟--------------");
 	}
 
-	public XsapConfig(Long gap_minute) {
-		super();
-		this.gap_minute = gap_minute;
-		System.out.println(gap_minute+"洒洒水分钟--------------");
-	}
-
-	
 	public Long getGap_minute() {
 		return gap_minute;
 	}
-	
+
 	public void setGap_minute(Long gap_minute) {
-		System.out.println("--------------");
-		System.out.println(gap_minute);
 		this.gap_minute = gap_minute;
 	}
 }
